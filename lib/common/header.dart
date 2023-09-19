dc0c5c08b939cc8082ab8b143e48f93946af96b2
soumyadghosh <soumyadeepghosh2004@zohomail.in>
2023-09-19 20:38:59 +0530

    modulated to reuse

--- lib/common/header.dart
+++ lib/common/header.dart
@@ -1,16 +1,29 @@
 // ignore: avoid_web_libraries_in_flutter
 import 'package:flutter/material.dart';
 import 'package:soumyadghosh_io/home_page.dart';
-import 'package:soumyadghosh_io/snap_page.dart';
-import '../constants.dart';
+import 'package:soumyadghosh_io/snap_maintained_page.dart';
+import 'constants.dart';
 import 'package:yaru_icons/yaru_icons.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 import 'package:soumyadghosh_io/click.dart';
+import 'package:soumyadghosh_io/snap_published_page.dart';
+import 'package:soumyadghosh_io/snap_contributed_page.dart';
+import 'popup_menu.dart';
 
 Widget appBar(BuildContext context) {
   final currentWidth = MediaQuery.of(context).size.width.toInt();
   final String desh, naam;
-
+  final theme = Theme.of(context);
+  final double fontSize;
+  if (currentWidth >= 1100) {
+    fontSize = 20;
+  } else if (currentWidth >= 600 && currentWidth < 1100) {
+    fontSize = 18;
+  } else if (currentWidth >= 485 && currentWidth < 600) {
+    fontSize = 16;
+  } else {
+    fontSize = 15;
+  }
   if (currentWidth < 600) {
     desh = '';
   } else {
@@ -46,20 +59,40 @@ Widget appBar(BuildContext context) {
         ],
       ),
       actions: [
-        Padding(
-          padding: const EdgeInsets.only(right: padding, left: padding),
+        PopupMenuButton(
+          itemBuilder: (context) => [
+            popupMenu(
+              'Published',
+              context,
+              theme,
+              fontSize,
+              (context) => const SnapPublishedPage(),
+            ),
+            popupMenu(
+              'Maintained',
+              context,
+              theme,
+              fontSize,
+              (context) => const SnapMaintainedPage(),
+            ),
+            popupMenu(
+              'Contributed',
+              context,
+              theme,
+              fontSize,
+              (context) => const SnapContributedPage(),
+            ),
+          ],
           child: YaruIconButton(
-            icon: const Icon(YaruIcons.snapcraft),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const SnapPage(),
-                ),
-              );
-            },
+            icon: Icon(
+              YaruIcons.snapcraft,
+              color: theme.brightness == Brightness.light
+                  ? Colors.black
+                  : Colors.white,
+            ),
           ),
         ),
+        const Padding(padding: EdgeInsets.all(padding / 5)),
         Padding(
           padding: const EdgeInsets.only(right: padding),
           child: YaruIconButton(
