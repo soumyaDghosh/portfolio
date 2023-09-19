2982eddd0345b0ee6ed017d3c6f6d96d8b9cb06c
soumyadghosh <soumyadeepghosh2004@zohomail.in>
2023-09-19 20:37:31 +0530

    moved this under common

---
+++ lib/common/constants.dart
@@ -0,0 +1,19 @@
+//ignore: avoid_web_libraries_in_flutter
+
+const padding = 20.0;
+
+const website = 'https://soumyadghosh.github.io/portfolio';
+
+const telegram = 'https://telegram.me/soumyatheman';
+
+const avatar = 'https://i.imgur.com/zgynPjC.jpg';
+
+const gitlab = 'https://gitlab.com/soumyaDghosh';
+
+const discord = 'https://discord.com/users/soumyaDghosh';
+
+const bharatVarsha = 'https://www.mea.gov.in/Images/pdf1/Part1.pdf';
+
+
+var aboutMe =
+    "My name is Soumyadeep Ghosh(সৌম্যদীপ ঘোষ), a linux and FOSS enthusiast from 'Bharat'. I am interested in many projects, including the Project Snap by Canonical. I have been working on Snapping many applications. Until ${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}, I have snapped, 50+ applications. ";
